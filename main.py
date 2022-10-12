8c3dd4edc5344e158448132dda9bea1b01d9ca21
Rafael Santos <contactrafaelsantos@gmail.com>
2022-10-12 15:48:48 -0300

    Main code init

---
+++ main.py
@@ -0,0 +1,24 @@
+#Program to automate update and upgrade of linux system.
+
+import os
+from colorama import Back, Fore
+import time
+
+print(Fore.BLUE + "Updating packages...")
+time.sleep(2)
+os.system("sudo apt-get update")
+print(Fore.BLUE + "Upgrading...")
+time.sleep(2)
+os.system("sudo apt-get full-upgrade")
+
+rmv_packages = str(input("Sometimes some packages are automatically installed and not really required. Do you want to remove them? (y/n): "))
+
+if(rmv_packages == "y"):
+    print("Removing not required packages...")
+    time.sleep(1)
+    os.system("sudo apt-get autoremove")
+
+print(Fore.RED + "Cleaning up terminal...")
+time.sleep(2)
+os.system("clear")
+
